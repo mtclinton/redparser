62e4da5fe098eb4b44a03feacdf6463749c3a6f4
mtclinton <maxtclinton@gmail.com>
2022-03-06 19:45:28 -0500

    more timestamp tests

--- tests/test_timestamps.py
+++ tests/test_timestamps.py
@@ -41,7 +41,7 @@ def test_timestamps_timestamp_log(test_log_file):
     assert len(result_list) == 6
 
 # memory tests
-def test_timestamps_test_log(test_log_file):
+def test_memory_timestamps_test_log(test_log_file):
     """Test that we are getting right data for timestamp lines. right now all logs have timestamps so should be 2000"""
     with open('./tests/test.log', 'rb') as file_obj:
         file_obj = file_obj.read()
@@ -51,7 +51,7 @@ def test_timestamps_test_log(test_log_file):
         result_list = result.splitlines()
     assert len(result_list) == 2000
 
-def test_timestamps_timestamp_log(test_log_file):
+def test_memory_timestamps_timestamp_log(test_log_file):
     """Test that we are getting right data for timestamp lines. right now all logs have timestamps so should be 2000"""
     with open('./tests/timestamp.log', 'rb') as file_obj:
         file_obj = file_obj.read()
